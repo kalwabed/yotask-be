b936fc0d28617d78cc5070e64128c5217a8bb2ce
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-02-19 17:20:36 +0700

    ref: env port

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
+const PORT = process.env.PORT || 7000
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
@@ -15,7 +17,7 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, swaggerConfig);
 	SwaggerModule.setup("swagger", app, document);
 
-	await app.listen(8000, "0.0.0.0");
+	await app.listen(PORT, "0.0.0.0");
 	console.log(`Application is runnning on: ${await app.getUrl()}`);
 }
 
